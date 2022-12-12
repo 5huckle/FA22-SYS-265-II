411a8d6e86ed2fba617f94a719d3798d07cddc6b
5huckle <jibreel.hamiddiaz@mymail.champlain.edu>
2022-12-12 07:03:44 +0000

    secure-ssh

--- linux/centos7/secure-ssh.sh
+++ linux/centos7/secure-ssh.sh
@@ -1,6 +1,13 @@
+#!/bin/bash
 #secure-ssh.sh
 #author 5huckle inspired by gmcyber
 #creates a new ssh user using $1 parameter
 #adds a public key from the local repo or curled from the remote repo
 #removes roots ability to ssh in
-echo "ALL YOUR CODE GOES HERE
+sudo useradd -m -d /home/$1 -s /bin/bash $1
+sudo mkdir /home/$1/.ssh
+sudo cp /home/jibreel/FA22-SYS-265-II/linux/public-keys/id_rsa.pub /home/$1/.ssh/authorized_keys
+sudo chmod 700 /home/$1/.ssh
+sudo chmod 600 /home/$1/.ssh/authorized_keys
+sudo chown -R $1:$1 /home/$1/.ssh
+
